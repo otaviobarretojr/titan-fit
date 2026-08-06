535bd7157f01e8f4cb349aa564f7eff5d1536063
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-06 11:05:17 -0400

    style(cardio): load cardio styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 import './styles/tokens.css';
 import './styles/global.css';
 import './styles/history.css';
+import './styles/cardio.css';
 import './styles/pwa.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
