045d59bd2188a55ccdb5136f6065f92741dc7b60
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-06 09:06:00 -0400

    style: include PWA controls stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import './styles/tokens.css';
 import './styles/global.css';
+import './styles/pwa.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
