523656c6ce5445eebe6709c3d7434ff1793447e3
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-08 11:11:22 -0400

    feat: load programming styles

--- src/main.tsx
+++ src/main.tsx
@@ -38,6 +38,7 @@ import './styles/titan-light-v031.css';
 import './styles/visual-harmony-v032.css';
 import './styles/full-visual-audit-v0321.css';
 import './styles/score-alignment-v0322.css';
+import './styles/programming-v033.css';
 
 enableTitanHaptics();
 enableEvolutionFeedback();
