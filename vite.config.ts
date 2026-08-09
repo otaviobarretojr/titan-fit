d1478f92d5104ac96e75ed07002731041252527e
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-09 14:40:10 -0400

    chore: preserve current PWA cache version during data migration

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        cacheId: 'titan-fit-v0.40.0',
+        cacheId: 'titan-fit-v0.37.0',
         cleanupOutdatedCaches: true,
         navigateFallback: '/titan-fit/index.html',
         globPatterns: ['**/*.{js,css,html,svg,png}'],
