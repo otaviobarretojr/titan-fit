ff5d8dedbb4a990a0d0fae949dabfa1d5e6f2449
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-10 13:02:44 -0400

    feat: register Samsung Health native plugin

--- android/app/src/main/java/com/otaviobarretojr/titanfit/MainActivity.kt
+++ android/app/src/main/java/com/otaviobarretojr/titanfit/MainActivity.kt
@@ -3,10 +3,12 @@ package com.otaviobarretojr.titanfit
 import android.os.Bundle
 import com.getcapacitor.BridgeActivity
 import com.otaviobarretojr.titanfit.health.TitanHealthConnectPlugin
+import com.otaviobarretojr.titanfit.health.TitanSamsungHealthPlugin
 
 class MainActivity : BridgeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         registerPlugin(TitanHealthConnectPlugin::class.java)
+        registerPlugin(TitanSamsungHealthPlugin::class.java)
         super.onCreate(savedInstanceState)
     }
 }
