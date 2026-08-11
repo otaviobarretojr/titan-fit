7b38e0765d8ea278c70ca57a3209739153df1178
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-10 21:36:02 -0400

    style: load compact next-meal home treatment

--- src/main.tsx
+++ src/main.tsx
@@ -60,6 +60,7 @@ import './styles/nutrition-health-v053.css';
 import './styles/nutrition-program-clean-v058.css';
 import './styles/home-compact-v058.css';
 import './styles/navigation-sync-v0571.css';
+import './styles/home-nutrition-next-v0572.css';
 
 enableTitanHaptics();
 enableEvolutionFeedback();
