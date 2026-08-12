0c76218d72ec7e59f38a67f9db44cb32ae63534d
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-12 17:53:13 -0400

    feat: mount Coach v5 adaptive day panel

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AdaptiveDayCoachPanel } from './app/AdaptiveDayCoachPanel';
 import { NutritionShell } from './app/NutritionShell';
 import { NutritionNotificationBridge } from './app/NutritionNotificationBridge';
 import './styles/tokens.css';
@@ -13,10 +14,12 @@ import './styles/nutrition-v4.css';
 import './styles/nutrition-v5.css';
 import './styles/nutrition-v6.css';
 import './styles/nutrition-v7.css';
+import './styles/nutrition-v8.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NutritionNotificationBridge />
     <NutritionShell />
+    <AdaptiveDayCoachPanel />
   </React.StrictMode>
 );
