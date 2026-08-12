ff9b7b0d8ba07ec3b7804262207f6f7d529dddd8
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-12 13:01:38 -0400

    style(nutrition): load v3 styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './styles/global.css';
 import './styles/nutrition.css';
 import './styles/nutrition-sections.css';
 import './styles/nutrition-v2.css';
+import './styles/nutrition-v3.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
